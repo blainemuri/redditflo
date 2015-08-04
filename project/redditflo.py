45575a6e953e88ccaa2b3dcada7fb500bd53783e
Blaine Muri <blainemuri@Blaines-MacBook-Pro-2.local>
2015-08-04 14:37:33 -0700

    more attempts, still nothing working

--- project/redditflo.py
+++ project/redditflo.py
@@ -1,47 +1,85 @@
-from flask import Flask, render_template, request, url_for, abort
-from uuid import uuid4
-import requests.auth, urllib, urllib.parse, json, requests, praw
+# from flask import Flask, render_template, request, url_for, abort
+# from uuid import uuid4
+# from praw.errors import HTTPException
+# import requests.auth, urllib, urllib.parse, json, requests, praw
 
-app = Flask(__name__, static_folder='static')
+# app = Flask(__name__, static_folder='static')
+
+from flask import Flask, request, render_template
+
+import praw
+
+app = Flask(__name__)
 
 CLIENT_ID = 'HIkG7mKev6WoNA'
 CLIENT_SECRET = 'pXDTQ-whb9tQWt-QN2wBU_oHB1A'
-REDIRECT_URI = "http://localhost:5000/homepage.html"
+REDIRECT_URI = "http://localhost:5000/authorize_callback"
 
 @app.route('/')
 def homepage():
-  key = str(uuid4())
-  link_refresh = r.get_authorize_url(key, refreshable=True)
-  return render_template('login.html', link=link_refresh)
+    link_no_refresh = r.get_authorize_url('UniqueKey')
+    link_refresh = r.get_authorize_url('DifferentUniqueKey',
+                                       refreshable=True)
+    link_no_refresh = "<a href=%s>link</a>" % link_no_refresh
+    link_refresh = "<a href=%s>link</a>" % link_refresh
+    text = "First link. Not refreshable %s</br></br>" % link_no_refresh
+    text += "Second link. Refreshable %s</br></br>" % link_refresh
+    return text
 
-@app.route('/homepage.html')
+@app.route('/authorize_callback')
 def authorized():
-  state = request.args.get('state', '')
-  code = request.args.get('code', '')
-  info = r.get_access_information(code)
-  user = r.get_me()
-  variables_text = "State=%s, code=%s, info=%s." % (state, code,
-                                                    str(info))
-  text = 'You are %s and have %u link karma.' % (user.name,
-                                                 user.link_karma)
-  return render_template('/homepage.html')
+    state = request.args.get('state', '')
+    code = request.args.get('code', '')
+    info = r.get_access_information(code)
+    user = r.get_me()
+    variables_text = "State=%s, code=%s, info=%s." % (state, code,
+                                                      str(info))
+    text = 'You are %s and have %u link karma.' % (user.name,
+                                                   user.link_karma)
+    back_link = "<a href='/'>Try again</a>"
+    return render_template('homepage.html')
 
 if __name__ == '__main__':
-  r = praw.Reddit('OAuth2 verification for use by u/blaine1726 ver 0.1 see '
-                  'https://github.com/blaine1726/redditflo for source')
-  r.set_oauth_app_info(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
-  app.run(debug=True, port=5000)
+    r = praw.Reddit('OAuth Webserver example by u/_Daimon_ ver 0.1. See '
+                    'https://praw.readthedocs.org/en/latest/'
+                    'pages/oauth.html for more info.')
+    r.set_oauth_app_info(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
+    app.run(debug=True, port=5000)
+
+##################################################
+### This is the Praw way of going about things ###
+
+# @app.route('/')
+# def initial():
+#   link_refresh = r.get_authorize_url(USER_KEY, refreshable=True)
+#   return render_template('login.html', link=link_refresh)
+
+# @app.route('/authorize_callback')
+# def authorized():
+#   try:
+#     state = request.args.get('state', '')
+#     code = request.args.get('code', '')
+#     info = r.get_access_information(code, update_session=True)
+#     user = r.get_me()
+#     variables_text = "State=%s, code=%s, info=%s." % (state, code,
+#                                                       str(info))
+#     text = 'You are %s and have %u link karma.' % (user.name,
+#                                                    user.link_karma)
+#   except HTTPException as x:
+#     print(x)
+#   return render_template('homepage.html')
+
+# if __name__ == '__main__':
+#   r = praw.Reddit('OAuth2 verification for use by u/blaine1726 ver 0.1 see '
+#                   'https://github.com/blaine1726/redditflo for source')
+#   r.set_oauth_app_info(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
+#   app.run(debug=True, port=5000)
+
+### End of praw example ###
+###########################
 
 # @app.route('/')
 # def login():
-#   r = praw.Reddit('OAuth2 verification for use by u/blaine1726 ver 0.1 see '
-#                   'https://github.com/blaine1726/redditflo for source')
-#   r.set_oauth_app_info(client_id= CLIENT_ID,
-#                        client_secret= CLIENT_SECRET,
-#                        redirect_uri= REDIRECT_URI)
-#   url = r.get_authorize_url('uniqueKey', 'identity', True)
-#   import webbrowser
-#   webbrowser.open(url)
 
 #   state = str(uuid4())
 #   access_information = r.get_access_information(state)
